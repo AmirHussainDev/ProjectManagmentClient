9a37e0885138e2af4526be497ab7edb980810ad6
root <amir.hussain.developer@gmail.com>
2024-04-28 01:51:50 +0500

    fix ui as per zoho

--- dist/veins-frontend/browser/chunk-NDVZETOL.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-SN7HD7MG.js";import"./chunk-NGE6N3H6.js";import"./chunk-733G2MTE.js";import"./chunk-YR6QF5UB.js";import"./chunk-NFQMHT45.js";import"./chunk-MON7YFGF.js";export{a as ShellModule};
---
+++ dist/veins-frontend/browser/chunk-UHGQNXVG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-Q7SSU4YG.js";import"./chunk-NGE6N3H6.js";import"./chunk-BQGCRMRC.js";import"./chunk-NFQMHT45.js";import"./chunk-MON7YFGF.js";export{a as LoginModule};
