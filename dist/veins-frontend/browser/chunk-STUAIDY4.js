eb9a61a119ca385c7b0e730469f7f02c6b373d2d
root <amir.hussain.developer@gmail.com>
2024-06-06 22:37:54 +0500

    fix app service

---
+++ dist/veins-frontend/browser/chunk-STUAIDY4.js
@@ -0,0 +1,1 @@
+import{E as n,x as t}from"./chunk-LS5T5ZRO.js";var y=(()=>{class e{static \u0275fac=function(i){return new(i||e)};static \u0275mod=t({type:e});static \u0275inj=n({})}return e})();export{y as a};
