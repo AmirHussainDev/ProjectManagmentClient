84e9486abc5394a09d9ded82bbfe0620ecf89e80
root <amir.hussain.developer@gmail.com>
2024-08-16 00:19:33 +0500

    fix front end issues

--- dist/veins-frontend/browser/chunk-O7YADBHG.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-U6ZUXRIG.js";import"./chunk-NGE6N3H6.js";import"./chunk-KHMPBDQ2.js";export{a as LoginModule};
---
+++ dist/veins-frontend/browser/chunk-UMVC5H4V.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-QYONNLI4.js";import"./chunk-NGE6N3H6.js";import"./chunk-YAX6DQEX.js";export{a as LoginModule};
