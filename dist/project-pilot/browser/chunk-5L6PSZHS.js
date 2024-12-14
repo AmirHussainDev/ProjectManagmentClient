ba9683e7c22f4ac3cf0b48641c3bf133baf26410
AmirHussainDev <amir.hussain.developer@gmail.com>
2024-12-15 02:15:41 +0500

    fix ui issues

--- dist/project-pilot/browser/chunk-5L6PSZHS.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-H5X5S5PB.js";import"./chunk-NGE6N3H6.js";import"./chunk-RYV5VKQF.js";export{a as LoginModule};
---
+++ dist/project-pilot/browser/chunk-QJLQF7CC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-FFEB2OB6.js";import"./chunk-NGE6N3H6.js";import"./chunk-RYV5VKQF.js";export{a as LoginModule};
